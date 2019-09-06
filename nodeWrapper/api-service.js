2aa99d82a2ab011f03482c00c6e339113f878b5e
Vincent Chho <vincec362@gmail.com>
2019-09-06 12:04:18 -0700

    update works and refactored nodeWrapper

--- nodeWrapper/api-service.js
+++ nodeWrapper/api-service.js
@@ -25,28 +25,50 @@ exports.getAllProducts = (callback) => {
     });
 }
 
-exports.getProductById = (id, callback) => {
-    console.log(`id within the NodeJSCall`, id);
-    http.get(_EXTERNAL_URL_GET + id, (response) => {
+// exports.getProductById = (id, callback) => {
+//     console.log(`id within the NodeJSCall`, id);
+//     http.get(_EXTERNAL_URL_GET + id, (response) => {
 
-        // const myURL = new URL(_EXTERNAL_URL_GET + id);
-        let data = '';
+//         // const myURL = new URL(_EXTERNAL_URL_GET + id);
+//         let data = '';
 
-        response.on('data', (chunk) => {
-            data += chunk;
+//         response.on('data', (chunk) => {
+//             data += chunk;
+//         });
+
+//         response.on('end', () => {
+//             console.log(JSON.stringify(data));
+//             return callback(data);
+//         })
+//     }).on('error', (err) => {
+//         console.log(`Error: ${err.message}`);
+//     })
+// }
+
+exports.getProductById = (id) => {
+    return new Promise ((resolve, reject) => {
+        http.get(_EXTERNAL_URL_GET + id, (resp) => {
+            let data = '';
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => { data += chunk; });
+        
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(data);
+                    return resolve(data);
+                } catch (e) {
+                    console.error(e.message);
+                }
+            });
+        }).on('error', (err) => {
+            // console.log("Error: " + err.message);
+            console.log(`Error: ${err.message}`);
         });
-
-        response.on('end', () => {
-            console.log(JSON.stringify(data));
-            return callback(data);
-        })
-    }).on('error', (err) => {
-        console.log(`Error: ${err.message}`);
     })
 }
 
 exports.addProduct = (callback) => {
-    
     const product = JSON.stringify({
         Title: 'hello from node',
         Price: 20,
@@ -123,15 +145,45 @@ exports.deleteProduct = (callback,id) => {
 }
 
 exports.updateProduct = (id) => {
-    // console.log(getSingleProduct(id, callback));
+    // const product = JSON.stringify(callback);
 
-    // let singleProduct =  getSingleProductPromise(id);
-    // console.log(`Single product`, singleProduct);
-    let work = pleaseWork(id);
-    console.log(`work? `, work);
+    const product = JSON.stringify({
+        Title: 'Mega Man Battle Network',
+        Price: 4000,
+        ImageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cb/Megamanbattlenetwork_boxshot.jpg/220px-Megamanbattlenetwork_boxshot.jpg',
+        Description: 'Mega Man Battle Network is a video game developed by Capcom for the Game Boy Advance (GBA) handheld console. It is the first title of the Mega Man Battle Network series of games. It was originally released in Japan as a GBA launch game on March 21, 2001 and was released later that year in North America and Europe. It was also released via the Wii U Virtual Console in Japan on July 9, 2014, in Europe on July 24, 2014, and in North America on July 31, 2014.'
+    });
+
+    const options = {
+        hostname: 'localhost',
+        port: 50518,
+        path: _EXTERNAL_URL_GET + id,
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(product)
+        }
+    }
+
+    const req = http.request(options, (response) => {
+        console.log(`Status: ${response.statusCode}`);
+        console.log(`Headers: ${JSON.stringify(response.headers)}`);
+        response.setEncoding('utf8');
+        response.on('data', (chunk) => {
+            console.log(`Body: ${chunk}`);
+        });
+        response.on('end', () => {
+            return response;
+            // console.log('No more data in response.');
+        });
+    });
+    
+    req.on('error', (err) => {
+        console.error(`problem with request: ${err.message}`);
+    });
+
+    req.write(product);
+    req.end();
 }
-
-
-
 
 // module.exports.callApi = callExternalApiUsingHttp;
--- nodeWrapper/app.js
+++ nodeWrapper/app.js
@@ -1,93 +1,35 @@
-const apiCallFromNode = require('./apiController')
+const apiService = require('./api-service')
 
 const http = require('http');
 const url = require('url');
 
 http.createServer((req, res) => {
-    
     let urlObj = url.parse(req.url, true);
     let id = urlObj.pathname.split("/")[2];
 
     if (urlObj.path === '/') {
-        apiCallFromNode.getAllProducts(function(response){
+        apiService.getAllProducts(function(response){
             res.write(response);
             res.end();
         });
     } else if (urlObj.path === `/getById/${id}`) {
-
         console.log(`id within getById route: `, id);
-
-        apiCallFromNode.getProductById(id, function(response){
-            res.write(response);
+        apiService.getProductById(id).then(result => {
+            res.write(result);
             res.end();
         });
     } else if (urlObj.path === '/addProduct') {
-        apiCallFromNode.addProduct(function(response){
+        apiService.addProduct(function(response){
             // console.log(`response in app.js: `,response);
             // res.write(response);
             // res.end();
         });
     } else if (urlObj.path === `/updateProduct/${id}`) {
-        let test;
-        http.get(_EXTERNAL_URL_GET + id, (resp) => {
-            const getSingleProductPromise = new Promise ((resolve, reject ) => {
-                
-                    let data = '';
-                    // A chunk of data has been recieved.
-                    resp.on('data', (chunk) => { data += chunk; });
-                
-                    // The whole response has been received. Print out the result.
-                    resp.on('end', () => {
-                        try {
-                            const parsedData = JSON.parse(data);
-                            console.log(`From getSingleProduct`, parsedData);
-                            return resolve(data);
-                        } catch (e) {
-                            console.error(e.message);
-                        }
-                        return getSingleProductPromise;
-                    });
-            })
-            .then(result => {
-                test=result;
-                console.log(`Test`,test);
-            })
-                
-        }).on('error', (err) => {
-            // console.log("Error: " + err.message);
-            console.log(`Error: ${err.message}`);
-        });
-
-        
+        apiService.updateProduct(id);
     } else if (urlObj.path === `/deleteProduct/${id}`) {
-        let productToBeDeleted;
-        http.get(_EXTERNAL_URL_GET + id, (resp) => {
-            const getSingleProductPromise = new Promise ((resolve, reject ) => {
-                
-                    let data = '';
-                    // A chunk of data has been recieved.
-                    resp.on('data', (chunk) => { data += chunk; });
-                
-                    // The whole response has been received. Print out the result.
-                    resp.on('end', () => {
-                        try {
-                            const parsedData = JSON.parse(data);
-                            // console.log(`From getSingleProduct`, parsedData);
-                            return resolve(data);
-                        } catch (e) {
-                            console.error(e.message);
-                        }
-                        return getSingleProductPromise;
-                    });
-            })
-            .then(result => {
-                productToBeDeleted=result;
-                apiCallFromNode.deleteProduct(productToBeDeleted, id);
-            })
-                
-        }).on('error', (err) => {
-            // console.log("Error: " + err.message);
-            console.log(`Error: ${err.message}`);
+        apiService.getProductById(id).then(result => {
+            console.log(`Product deleted: `, result);
+            apiService.deleteProduct(result, id);
         });
     }
 
